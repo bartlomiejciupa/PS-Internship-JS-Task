d65d23a546c31625d0b875efac46656d742b7a51
Bartlomiej Ciupa <bartek-ciupa@o2.pl>
2022-06-04 01:17:14 +0200

    improving styling wrapper class added function showNews created reset.css added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const userInput = window.document.querySelector('.nb-news-input');
-const newsList = window.document.querySelector('.news-list');
+const userInput = document.querySelector('.nb-news-input');
+const newsList = document.querySelector('.news-list');
 
 const loading = document.querySelector('.loading');
 userInput.addEventListener('change', loadNews);
@@ -17,7 +17,30 @@ function showLoading() {
   loading.classList.add('show');
   setTimeout(loadNews, 1000);
 }
+const showNews = (data) => {
+  data.forEach((news) => {
+    let wrapper = document.createElement('div');
+    let newsTitle = document.createElement('h2');
+    let summaryField = document.createElement('p');
+    let link = document.createElement('a');
 
+    wrapper.classList.add('wrapper');
+    link.textContent = 'Read article';
+    link.setAttribute('href', news.url);
+    link.setAttribute('target', '_blank');
+    newsTitle.textContent = news.title;
+
+    if (news.summary.length - 200 <= 0) {
+      summaryField.textContent = news.summary + ' ...';
+    } else {
+      summaryField.textContent = news.summary.substring(0, 200) + ' ...';
+    }
+    wrapper.append(newsTitle, summaryField, link);
+    newsList.appendChild(wrapper);
+
+    loading.classList.remove('show');
+  });
+};
 function loadNews(e) {
   let newsNumber = userInput.value;
 
@@ -28,27 +51,7 @@ function loadNews(e) {
       return response.json();
     })
     .then((data) => {
-      data.forEach((news) => {
-        let newsTitle = document.createElement('h2');
-        let summaryField = document.createElement('p');
-        let link = document.createElement('a');
-
-        link.textContent = 'Read more';
-        link.setAttribute('href', news.url);
-        link.setAttribute('target', '_blank');
-        newsTitle.textContent = news.title;
-
-        if (news.summary.length - 200 <= 0) {
-          summaryField.textContent = news.summary + ' ...';
-        } else {
-          summaryField.textContent = news.summary.substring(0, 200) + ' ...';
-        }
-
-        newsList.appendChild(newsTitle);
-        newsList.appendChild(summaryField);
-        newsList.appendChild(link);
-        loading.classList.remove('show');
-      });
+      showNews(data);
     })
     .catch((error) => {
       console.log(error);
