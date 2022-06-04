35f004e3053bdd189080244c2f85a102a0b1636c
Bartlomiej Ciupa <bartek-ciupa@o2.pl>
2022-06-04 04:24:23 +0200

    feat: counter intro

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 const userInput = document.querySelector('.nb-news-input');
 const newsList = document.querySelector('.news-list');
-
 const loading = document.querySelector('.loading');
+const container = document.querySelector('.container');
+
 userInput.addEventListener('change', loadNews);
 
 window.addEventListener('scroll', () => {
@@ -13,6 +14,16 @@ window.addEventListener('scroll', () => {
     showLoading();
   }
 });
+
+let newsNumber;
+if (userInput.value) {
+  newsNumber = userInput.value;
+} else {
+  newsNumber = 15;
+}
+console.log(newsNumber);
+console.log(userInput.value);
+
 function showLoading() {
   loading.classList.add('show');
   setTimeout(loadNews, 1000);
@@ -31,27 +42,36 @@ const showNews = (data) => {
     newsTitle.textContent = news.title;
 
     if (news.summary.length - 200 <= 0) {
-      summaryField.textContent = news.summary + ' ...';
+      summaryField.textContent = news.summary + '...';
     } else {
-      summaryField.textContent = news.summary.substring(0, 200) + ' ...';
+      summaryField.textContent = news.summary.substring(0, 200) + '...';
     }
     wrapper.append(newsTitle, summaryField, link);
     newsList.appendChild(wrapper);
-
     loading.classList.remove('show');
   });
 };
-function loadNews(e) {
-  let newsNumber = userInput.value;
+const showCounter = (data) => {
+  const counter = document.createElement('span');
+  counter.classList.add('articles-counter');
+  counter.innerText = 'Number of articles: ' + data;
+  container.appendChild(counter);
+  let counterLoadedNews = document.createElement('p');
+  counterLoadedNews.textContent = newsNumber;
+  counter.appendChild(counterLoadedNews);
+};
 
+function loadNews() {
   let url = `https://api.spaceflightnewsapi.net/v3/articles?_limit=${newsNumber}`;
 
+  let urlTest = 'https://api.spaceflightnewsapi.net/v3/articles';
   fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       showNews(data);
+      console.log(data.length);
     })
     .catch((error) => {
       console.log(error);
@@ -59,4 +79,17 @@ function loadNews(e) {
 }
 loadNews();
 
-module.exports = { summaryField };
+function loadCounter() {
+  let url = `https://api.spaceflightnewsapi.net/v3/articles/count`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      showCounter(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+loadCounter();
